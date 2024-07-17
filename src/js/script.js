f23df2faa3299c603991e1c8beb10bb8e97b0649
Kanan <kanansnote@gmail.com>
2024-07-16 18:22:07 -0700

    Added JS DOM related tasks from 1 to 3.

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+// Task 1
+
+// const fruitList = ["apple", "banana", "tomato"];
+// const ul = document.createElement('ul');
+// const body = document.querySelector('body');
+
+// fruitList.forEach(item=> {
+//     const li = document.createElement('li');
+//     li.textContent = item;
+//     ul.appendChild(li);
+// });
+
+// body.appendChild(ul);
+
+// Task 2
+
+// const image = document.createElement('img');
+// const body = document.querySelector('body');
+// const div = document.createElement('div');
+// const imageSrc = 'https://crocoder.dev/icon.png';
+
+// image.src = imageSrc;
+// div.appendChild(image);
+// body.appendChild(div);
+
+// document.body.appenchild(div) and we don't need body tag selected.
+
+// Task 3
+
+// const ulLi1 = document.querySelectorAll('li:nth-child(1)');
+// const ulLi3 = document.querySelectorAll('li:nth-child(3)');
+
+// ulLi1.forEach(item=>item.textContent='first');
+// ulLi3.forEach(item=>item.textContent='third');
